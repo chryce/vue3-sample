0fd0fbbd62b85206253f4a9dbee8274c5458a460
胡睿毅 <807776962@qq.com>
2022-06-12 21:43:14 +0800

    vue-route

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <router-link to="/">Home</router-link>
+    &nbsp;
+    <router-link to="/hello">Hello</router-link>
+    &nbsp;
+    <router-link to="/user">User</router-link>
+    &nbsp;
+    <router-link to="/details">Details</router-link>
+    &nbsp;
+    <router-link to="/about">About</router-link>
+  </div>
+  <div>
+    <router-view></router-view>
+  </div>
+
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
