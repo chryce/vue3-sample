4764ff7abc23552b4dc6aa673e7ca6b4e13ed82a
胡睿毅 <807776962@qq.com>
2022-07-28 20:40:35 +0800

    时间测试

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     About Page
+    <div>Current Date:{{date}}</div>
   </div>
 </template>
 
 <script>
+import moment from "moment/moment";
+
 export default {
-  name: "about-page"
+  name: "about-page",
+  data() {
+    return {
+      date: null,
+    }
+  },
+  mounted() {
+    this.date=moment()
+  }
 }
 </script>
 
